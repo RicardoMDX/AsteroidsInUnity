1fce00fefd01fab537ec39a8be207d24a812081c
Unknown <rh845@live.mdx.ac.uk>
2019-09-30 18:38:22 +0100

    Player Base

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed, maxSpeed = 2f, turnRate=20f;
+    private Rigidbody2D playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB=GetComponent<Rigidbody2D>();
+        speed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Input.GetKey("W");
+
+    }
+}
